131c238c11a6996866c8ccab9594bcd1689f5b5c
unknown <ja.suarezg496@gmail.com>
2022-11-24 17:27:56 -0300

    embellecimiento formulario OK

--- formulario.php
+++ formulario.php
@@ -16,15 +16,15 @@ include("header.php");
 
 <div style="margin-inline: 20%;">
 <form method="POST" action="registrar.php" enctype="multipart/form-data">
-    <p>Nombre de empresa: <input style="float:right; margin-right:15%" type="text" placeholder="Nombre completo" name='nombre'></p>
-    <p>Correo electrónico: <input style="float:right; margin-right:15%" type="email" placeholder="Correo electrónico" name='correo'></p><br>
+    <p>Nombre de empresa: <input class="input-form" type="text" placeholder="Nombre completo" name='nombre'></p><br>
+    <p>Correo electrónico: <input class="input-form" type="email" placeholder="Correo electrónico" name='correo'></p><br>
 
-    <p>Imagen de referencia: <input style="float:right; margin-right:15%" type="file" name="file"></p><br>
-    <p>Título de la publicación: <input style="float:right; margin-right:15%" type="text" placeholder="Título del producto o servicio a ofrecer" name="ofrecimiento"></p><br>
+    <p>Imagen de referencia: <input class="input-form" type="file" name="file"></p><br>
+    <p>Título de la publicación: <input class="input-form" type="text" placeholder="Título del producto o servicio a ofrecer" name="ofrecimiento"></p><br><br>
     
-    Descripción: <br><textarea rows="10" cols="69" placeholder="Describa su producto o servicio" name="descripcion"></textarea><br>
-
-    <input type="submit" name="submit" value="Publicar">
+    Descripción: <br><textarea rows="10" cols="69" placeholder="Describa su producto o servicio" name="descripcion"></textarea>
+    <br><br>
+    <input class="button-form" type="submit" name="submit" value="Publicar">
 </form>
 </div>
 <br><br><br><br>
